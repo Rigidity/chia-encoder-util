dd72e6698f493d5e67528c251156a7e429034921
Rigidity <me@rigidnetwork.com>
2024-08-28 15:27:45 -0400

    Reproduce actual behavior

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,10 @@
 import { bech32m } from "bech32";
 import bls from "chia-bls";
+import clvm from "clvm-lib";
 import { useState } from "react";
 
-const { AugSchemeMPL, JacobianPoint } = bls;
+const { AugSchemeMPL, JacobianPoint, PrivateKey, bigIntToBytes, hash256 } = bls;
+const { Program } = clvm;
 
 export default function Main() {
   const [m, setM] = useState("");
@@ -27,11 +29,63 @@ export default function Main() {
   );
 }
 
+function pathInto(pk, nums) {
+  for (const num of nums) {
+    pk = AugSchemeMPL.deriveChildPkUnhardened(pk, num);
+  }
+  return pk;
+}
+
+const groupOrder =
+  0x73eda753299d7d483339d80809a1d80553bda402fffe5bfeffffffff00000001n;
+
+const defaultHidden = Program.deserializeHex("ff0980").hash();
+
+export function calculateSyntheticPublicKey(publicKey, hiddenPuzzleHash) {
+  return JacobianPoint.fromBytes(
+    Program.deserializeHex("ff1dff02ffff1effff0bff02ff05808080").run(
+      Program.fromList([
+        Program.fromJacobianPoint(publicKey),
+        Program.fromBytes(hiddenPuzzleHash),
+      ])
+    ).value.atom,
+    false
+  );
+}
+
+export function calculateSyntheticPrivateKey(privateKey, hiddenPuzzleHash) {
+  const privateExponent = bytesToBigInt(privateKey.toBytes(), "big");
+  const publicKey = privateKey.getG1();
+  const syntheticOffset = calculateSyntheticOffset(publicKey, hiddenPuzzleHash);
+  const syntheticPrivateExponent = mod(
+    privateExponent + syntheticOffset,
+    groupOrder
+  );
+  const blob = bigIntToBytes(syntheticPrivateExponent, 32, "big");
+  return PrivateKey.fromBytes(blob);
+}
+
+export function calculateSyntheticOffset(publicKey, hiddenPuzzleHash) {
+  const blob = hash256(concatBytes(publicKey.toBytes(), hiddenPuzzleHash));
+  return mod(decodeBigInt(blob), groupOrder);
+}
+
 function calc(hex) {
   try {
     const key = JacobianPoint.fromHexG1(hex.replace("0x", ""));
     const address = toAddress(
-      AugSchemeMPL.deriveChildPkUnhardened(key, 0).toBytes(),
+      Program.deserializeHex(
+        "ff02ffff01ff02ffff03ff0bffff01ff02ffff03ffff09ff05ffff1dff0bffff1effff0bff0bffff02ff06ffff04ff02ffff04ff17ff8080808080808080ffff01ff02ff17ff2f80ffff01ff088080ff0180ffff01ff04ffff04ff04ffff04ff05ffff04ffff02ff06ffff04ff02ffff04ff17ff80808080ff80808080ffff02ff17ff2f808080ff0180ffff04ffff01ff32ff02ffff03ffff07ff0580ffff01ff0bffff0102ffff02ff06ffff04ff02ffff04ff09ff80808080ffff02ff06ffff04ff02ffff04ff0dff8080808080ffff01ff0bffff0101ff058080ff0180ff018080"
+      )
+        .curry([
+          Program.fromJacobianPoint(
+            calculateSyntheticPublicKey(
+              pathInto(key, [12381, 8444, 2, 0]),
+              defaultHidden
+            )
+          ),
+        ])
+        .hash(),
       "bls1238"
     );
     return address;
