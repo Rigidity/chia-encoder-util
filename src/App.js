083eafca49b39140248c67d548159e144e174795
Rigidity <me@rigidnetwork.com>
2024-08-28 14:20:20 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import { bech32m } from "bech32";
+import { AugSchemeMPL, JacobianPoint } from "chia-bls";
+import { useState } from "react";
+import "./App.css";
+
+function calc(hex) {
+  try {
+    const key = JacobianPoint.fromHexG1(hex.replace("0x", ""));
+    const address = toAddress(
+      AugSchemeMPL.deriveChildPkUnhardened(key, 0).toBytes(),
+      "bls1238"
+    );
+    return address;
+  } catch (e) {
+    console.error(e);
+    return null;
+  }
+}
+
+export function toAddress(hash, prefix) {
+  console.log(hash, prefix);
+  return bech32m.encode(prefix, convertBits(hash, 8, 5, true), Infinity);
+}
+
+export function addressInfo(address) {
+  const { words, prefix } = bech32m.decode(address);
+  return {
+    hash: convertBits(Uint8Array.from(words), 5, 8, false),
+    prefix,
+  };
+}
+
+export function convertBits(bytes, from, to, pad) {
+  let accumulate = 0;
+  let bits = 0;
+  let maxv = (1 << to) - 1;
+  let result = [];
+  for (const value of bytes) {
+    const b = value & 0xff;
+    if (b < 0 || b >> from > 0) throw new Error("Could not convert bits.");
+    accumulate = (accumulate << from) | b;
+    bits += from;
+    while (bits >= to) {
+      bits -= to;
+      result.push((accumulate >> bits) & maxv);
+    }
+  }
+  if (pad && bits > 0) {
+    result.push((accumulate << (to - bits)) & maxv);
+  } else if (bits >= from || ((accumulate << (to - bits)) & maxv) !== 0) {
+    throw new Error("Could not convert bits.");
+  }
+  return Uint8Array.from(result);
+}
 
 function App() {
+  const [m, setM] = useState("");
+
+  const trimmed = m.trim();
+  const result = trimmed.length > 0 ? calc(trimmed) : null;
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <h1 style={{ fontSize: 40 }}>BLS Public Key Encoder</h1>
+        <input value={m} onChange={(e) => setM(e.target.value)}></input>
+        <p style={{ width: 700, wordWrap: "break-word" }}>
+          {m.trim().length > 0
+            ? result ?? "Invalid master public key"
+            : "Enter your master public key"}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p style={{ fontSize: 20 }}>
+          This converts a master public key to the first derivation encoded with
+          the bls12381 prefix.
+        </p>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
       </header>
     </div>
   );
